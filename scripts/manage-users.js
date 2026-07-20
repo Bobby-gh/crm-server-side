c6d6437154c51fb749df203d6ace6e5904f3d59f
Bobby-gh <knaihv@ymail.com>
2026-07-20 02:01:52 +0000

    Initial backend commit

--- scripts/manage-users.js
+++ scripts/manage-users.js
@@ -9,7 +9,18 @@ const path = require('path');
 const bcrypt = require('bcryptjs');
 const Database = require('better-sqlite3');
 
-const DB_PATH = path.join(__dirname, '..', 'data', 'wafi-crm.db');
+const Database = require('better-sqlite3');
+const path = require('path');
+const fs = require('fs');
+
+const DATA_DIR = path.join(__dirname, '..', 'data');
+
+if (!fs.existsSync(DATA_DIR)) {
+  fs.mkdirSync(DATA_DIR, { recursive: true });
+}
+
+const DB_PATH = path.join(DATA_DIR, 'wafi-crm.db');
+
 const db = new Database(DB_PATH);
 
 db.exec(`
